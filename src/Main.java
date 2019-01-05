de62b8275b7ba8c5e888ec5f9b09b0ad1de41663
Fornito Marvin <marvin.fornito@gmail.com>
2019-01-05 12:29:09 +0100

    Interface de l'invité de commande pemrettant de choisir l'algorithme à lancer.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main
 {
@@ -10,20 +11,48 @@ public class Main
         AldousBroder aldousBroder = new AldousBroder();
         Wilson wilson = new Wilson();
 
-        // Test pour 1 lancement de Kruskal
-        ArrayList<Edge> resultat = kruskal.getArbreCouvrant(graph);
-        kruskal.affichageKruskal(resultat);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Pour quel algorithme voulez-vous lancer l'étude de statistique ? \n " +
+                           "- Krusal : Entrer le numéro 1 \n " +
+                           "- Aldous-Brother : Entrer le numéro 2 \n " +
+                           "- Wilson : Entrer le numéro 3 \n");
+        boolean b = false;
+        int entier = -1;
+        entier = sc.nextInt();
+        if(entier == 1 || entier == 2 || entier == 3)
+        {
+            b = true;
+        }
+        while(b == false)
+        {
+            System.out.println("Mauvaise entrée, veuillez utiliser les chiffres 1, 2 ou 3 pour selectionner l'algorithme");
+            entier = sc.nextInt();
+            if(entier == 1 || entier == 2 || entier == 3)
+            {
+                b = true;
+            }
+        }
 
-        // Test pour 1000000 lancement de Kruskal
-        statistique.Lancement(kruskal, graph, 1000000).affiche();
+        if(entier == 1)
+        {
+            // Lancement pour 1000000 Kruskal
+            System.out.println("Lancement en cour... \n");
+            statistique.Lancement(kruskal, graph, 1000000).affiche();
+        }
+        if(entier == 2)
+        {
+            // Lancement pour 1000000 d'AldousBroder
+            System.out.println("Lancement en cour... \n");
+            statistique.Lancement(aldousBroder, graph, 1000000).affiche();
+        }
+        if(entier == 3)
+        {
+            // Lancement pour 1000000 de Wilson
+            System.out.println("Lancement en cour... \n");
+            statistique.Lancement(wilson, graph, 1000000).affiche();
+        }
 
-        // Test pour 1000000 lancement de AldousBroder
-        statistique.Lancement(aldousBroder, graph, 1000000).affiche();
-
-        // Test pour 1000000 lancement de Wilson
-        statistique.Lancement(wilson, graph, 1000000).affiche();
-
-        Labyrinthe l=new Labyrinthe(20, aldousBroder);
+        Labyrinthe l = new Labyrinthe(20, aldousBroder);
         System.out.println(Labyrinthe.nbMoyenCulSac(kruskal,2));
     }
 }
